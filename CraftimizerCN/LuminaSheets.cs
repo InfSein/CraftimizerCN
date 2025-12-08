63ffc52fb3b15aac938ec5b7a70ad36d15b4fd3c
InfSein <infsein@foxmail.com>
2025-12-08 21:15:18 +0800

    fixed, now should be available.

--- CraftimizerCN/LuminaSheets.cs
+++ CraftimizerCN/LuminaSheets.cs
@@ -5,9 +5,20 @@ using Lumina.Excel.Sheets;
 
 namespace CraftimizerCN.Plugin;
 
+#pragma warning disable IDE1006 // 命名样式
+
 public static class LuminaSheets
 {
-    private static readonly ExcelModule Module = Service.DataManager.GameData.Excel;
+    private static ExcelModule Module => Service.DataManager.Excel;
+    private static Language CurrLanguage
+    {
+        get
+        {
+            var lang = Service.DataManager.Language.ToLumina();
+            return lang == Language.None ? Language.ChineseSimplified : lang;
+        }
+    }
+    private static Language EnglishLanguage => Language.English;
 
     public static readonly ExcelSheet<Recipe> RecipeSheet = Module.GetSheet<Recipe>();
     public static readonly ExcelSheet<Action> ActionSheet = Module.GetSheet<Action>();
@@ -15,18 +26,12 @@ public static class LuminaSheets
     public static readonly ExcelSheet<Status> StatusSheet = Module.GetSheet<Status>();
     public static readonly ExcelSheet<Addon> AddonSheet = Module.GetSheet<Addon>();
     public static readonly ExcelSheet<ClassJob> ClassJobSheet = Module.GetSheet<ClassJob>();
-    public static ExcelSheet<Item> ItemSheet
-    {
-        get {
-            if (field == null)
-            {
-                var lang = Service.ClientState.ClientLanguage.ToLumina();
-                field = Module.GetSheet<Item>(lang)!;
-            }
-            return field;
-        }
-    }
-    public static readonly ExcelSheet<Item> ItemSheetEnglish = Module.GetSheet<Item>(Language.English)!;
+    public static ExcelSheet<Item> ItemSheet => field ??= Module.GetSheet<Item>(CurrLanguage)!;
+    private static readonly System.Lazy<ExcelSheet<Item>> _itemSheetEnglish = new(() => {
+        var lang = Dalamud.Game.ClientLanguage.English;
+        return Service.DataManager.GetExcelSheet<Item>(lang)!;
+    });
+    public static ExcelSheet<Item> ItemSheetEnglish => _itemSheetEnglish.Value;
     public static readonly ExcelSheet<Level> LevelSheet = Module.GetSheet<Level>();
     public static readonly ExcelSheet<Quest> QuestSheet = Module.GetSheet<Quest>();
     public static readonly ExcelSheet<Materia> MateriaSheet = Module.GetSheet<Materia>();
@@ -36,3 +41,5 @@ public static class LuminaSheets
     public static readonly ExcelSheet<RecipeLevelTable> RecipeLevelTableSheet = Module.GetSheet<RecipeLevelTable>();
     public static readonly ExcelSheet<GathererCrafterLvAdjustTable> GathererCrafterLvAdjustTableSheet = Module.GetSheet<GathererCrafterLvAdjustTable>();
 }
+
+#pragma warning restore IDE1006 // 命名样式
