812db100746be9ecd6608f2ba74dff81caac2afe
InfSein <infsein@foxmail.com>
2025-12-18 15:56:48 +0800

    Version 2.8.0.7 * more fit

--- CraftimizerCN/Service.cs
+++ CraftimizerCN/Service.cs
@@ -23,7 +23,7 @@ public sealed class Service
     [PluginService] public static IDataManager DataManager { get; private set; }
     [PluginService] public static ITextureProvider TextureProvider { get; private set; }
     [PluginService] public static IDalamudAssetManager DalamudAssetManager { get; private set; }
-    [PluginService] public static ITargetManager TargetManager { get; private set; }
+    //[PluginService] public static ITargetManager TargetManager { get; private set; }
     [PluginService] public static ICondition Condition { get; private set; }
     [PluginService] public static IFramework Framework { get; private set; }
     [PluginService] public static IPluginLog PluginLog { get; private set; }
